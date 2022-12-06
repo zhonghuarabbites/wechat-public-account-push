3ab94ab06661a33f2f97c75e6d92e6210e29bf6c
zhonghuarabbites <118323399+zhonghuarabbites@users.noreply.github.com>
2022-12-06 22:42:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '凡凡姐姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oehcn6kN3ZK8zBdYRkxbVBB2oozI','oehcn6o8ijKrPSCBOLqTLtbUlKb8'
+      id: 'oehcn6kN3ZK8zBdYRkxbVBB2oozI''oehcn6o8ijKrPSCBOLqTLtbUlKb8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'qL9XGPkFgw4ONV6muONY6-GiDg7KcldwqqORdRO_Pl8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
